2bcf9e2a6e6cb5d9b57281d863ba57fb19f86719
Leo <contato.leandrovmoreira@gmail.com>
2024-12-05 22:57:26 -0300

    parte estrutural do projeto

---
+++ main.py
@@ -0,0 +1,21 @@
+escolha_user = input( 
+       "[1] Adicionar Produto \n" 
+       "[2] Listar Produtos \n" 
+       "[3] Filtrar por categoria \n"
+       "[4] Ordenar produtos por preço \n"
+       "[5] Exibir Categorias \n"
+       "[6] Salvar Produtos \n"
+       "[7] Importar Produtos \n"
+       "[0] Sair \n"
+       "Escolha uma opção: "
+    )
+
+Produtos_cadastrados = {"nome": a,"categoria":  ,"preco": }
+
+def adicionar_produtos():
+    nome = input("Nome: ")
+    categoria = input("Nome: ")
+    preco = input("Nome: ")
+
+    Produtos_cadastrados.append(nome,categoria,preco)
+    
