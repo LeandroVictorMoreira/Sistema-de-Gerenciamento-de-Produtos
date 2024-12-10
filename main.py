243514e94236d7615c6ad4f749810bd89e3a74af
Leo <contato.leandrovmoreira@gmail.com>
2024-12-09 23:47:00 -0300

    Adição do escopo da função Adicionar

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
-escolha_user = input( 
-       "[1] Adicionar Produto \n" 
-       "[2] Listar Produtos \n" 
-       "[3] Filtrar por categoria \n"
-       "[4] Ordenar produtos por preço \n"
-       "[5] Exibir Categorias \n"
-       "[6] Salvar Produtos \n"
-       "[7] Importar Produtos \n"
-       "[0] Sair \n"
-       "Escolha uma opção: "
-    )
+Produtos_cadastrados = []
 
-Produtos_cadastrados = {"nome": a,"categoria":  ,"preco": }
 
 def adicionar_produtos():
     nome = input("Nome: ")
-    categoria = input("Nome: ")
-    preco = input("Nome: ")
+    categoria = input("Categoria: ")
+    preco = input("Preço: ")
+    produto = {'Nome': nome, 'Categoria': categoria, 'Preço': preco}
+    Produtos_cadastrados.append(produto)
+    retorno()
 
-    Produtos_cadastrados.append(nome,categoria,preco)
-    
+
+def retorno():
+    resposta_user = input("Deseja continuar adicionando ?\n")
+    if resposta_user == "s":
+        adicionar_produtos()
+    else:
+        listar_produtos()
+
+def listar_produtos():
+        for produto in Produtos_cadastrados:
+            nome = produto['Nome'] 
+            categoria = produto['Categoria']
+            preco = produto['Preço']
+            print(f"Nome: {nome},\nCategoria: {categoria}, \nPreço: R${preco}")
+
+
+def inicio ():
+    escolha_user = int(input( 
+    "[1] Adicionar Produto \n"           #Pendente
+    "[2] Listar Produtos \n"             #Pendente
+    "[3] Filtrar por categoria \n"       #Pendente
+    "[4] Ordenar produtos por preço \n"  #Pendente
+    "[5] Exibir Categorias \n"           #Pendente
+    "[6] Salvar Produtos \n"             #Pendente
+    "[7] Importar Produtos \n"           #Pendente
+    "[0] Sair \n"                        #Pendente
+    "Escolha uma opção: "
+    ))
+
+    if escolha_user == 1 :
+        adicionar_produtos()
+
+
+
+
+inicio()
+
+
+
+
+
